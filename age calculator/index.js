d7a3fb474fb018adbb347a4c9d3768d99ea876fa
Anvesh <136995186+findeme1@users.noreply.github.com>
2023-12-11 01:45:34 +0530

    first commit

--- age calculator/index.js
+++ age calculator/index.js
@@ -1,30 +1,36 @@
+//2
 const btnEl = document.getElementById("btn");
 const birthdayEl = document.getElementById("birthday");
 const resultEl = document.getElementById("result");
 
-function calculateAge(){
-    const birthdayValue = birthdayEl.value;
-    if(birthdayValue === ""){
-        alert("please enter your birthday");
-    }else{
-        const age = getAge(birthdayValue);
-        if(age < 0){
-            resultEl.innerText = `Invalid date of birth`;
-        }else{
-        resultEl.innerText  = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
-        }
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    if (age < 0) {
+      resultEl.innerText = `Invalid date of birth`;
+    } else {
+      resultEl.innerText = `Your age is ${age} ${
+        age > 1 ? "years" : "year"
+      } old`;
     }
+  }
 }
 
-function getAge(birthdayValue){
-    const currentDate = new Date();
-    const birthdayDate = new Date(birthdayValue);
-    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-    const month = currentDate.getMonth() - birthdayDate.getMonth();
-    if(month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())){
-        age--;
-    }
-    return age;
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+  return age;
 }
 
-btnEl.addEventListener("click",calculateAge);
+btnEl.addEventListener("click", calculateAge);
