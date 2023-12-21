5a8440a0d9affa3596c5bb69670b961266e89c84
Anvesh <136995186+findeme1@users.noreply.github.com>
2023-12-21 11:52:27 +0530

    added js part

---
+++ Blurred Background Poppup/index.js
@@ -0,0 +1,13 @@
+const btnEl = document.querySelector(".btn");
+const containerEl = document.querySelector(".container");
+const popupContainerEl = document.querySelector(".popup-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+btnEl.addEventListener("click",()=>{
+    containerEl.classList.add("active");
+    popupContainerEl.classList.remove("active");
+})
+closeIconEl.addEventListener("click",()=>{
+    popupContainerEl.classList.add("active");
+    containerEl.classList.remove("active");
+})
