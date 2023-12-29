94dc3e04220758697c35de269d6a56f451512ef6
Anvesh <136995186+findeme1@users.noreply.github.com>
2023-12-29 21:30:04 +0530

    Added Js

---
+++ Multiplication App/index.js
@@ -0,0 +1,29 @@
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+  score = 0;
+}
+scoreEl.innerText = `score: ${score}`;
+
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+  } else {
+    score--;
+  }
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
